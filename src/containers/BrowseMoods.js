67daa4f4102bfff318570e105b6e27851ab34ff7
mehmehmehlol <hkmeganlo@gmail.com>
2020-11-09 10:40:28 -0800

    changed something in browsemoods component

--- src/containers/BrowseMoods.js
+++ src/containers/BrowseMoods.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 
 export default class BrowseMoods extends Component {
+	
+	componentDidMount() {
+		fetch('http://localhost:3000/moods')
+		.then(res => res.json())
+		.then(moods => console.log(moods))
+	}
+	
 	render() {
-		return <div>BrowseMoods Component</div>;
+		return (
+			<div>
+				BrowseMoods Component
+			</div>
+		)
 	}
 }
