07fbc3440e5e3f151559d8b358efc526c6074ae9
candacecodes <candacehchoe@gmail.com>
2020-11-08 16:28:10 -0800

    fetch for random quote

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 
 export default class Quotes extends Component {
 	render() {
+		fetch(
+			"https://healthruwords.p.rapidapi.com/v1/quotes/?id=731&t=Wisdom&maxR=1&size=medium",
+			{
+				method: "GET",
+				headers: {
+					"x-rapidapi-key":
+						"dd160aebb6msh8564ece76aff9bbp10e534jsnf42b1b169f5f",
+					"x-rapidapi-host": "healthruwords.p.rapidapi.com",
+				},
+			}
+		).then((response) => {
+			console.log(response["url"]);
+		});
+
 		return <div>Quotes Component</div>;
 	}
 }
