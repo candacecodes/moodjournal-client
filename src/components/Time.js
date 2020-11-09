1429c8d50ae7b9a9b1a635676484467f41a4d271
candacecodes <candacehchoe@gmail.com>
2020-11-08 19:32:52 -0800

    centered/ bottom for time

--- src/components/Time.js
+++ src/components/Time.js
@@ -18,9 +18,18 @@ export default class Time extends Component {
 	}
 
 	render() {
+		const styles = {
+			position: "fixed",
+			left: "50%",
+			top: "50%",
+			"margin-top": "-50px",
+			"margin-left": "-100px",
+			"font-size": "30px",
+		};
+
 		return (
-			<div>
-				<h1>Hello, world!</h1>
+			<div style={styles}>
+				<h1>Hello, world</h1>
 				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
 			</div>
 		);
