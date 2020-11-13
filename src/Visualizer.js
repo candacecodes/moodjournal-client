8941023391f1627ec574df2a84ac844bce6fbcfb
candacecodes <candacehchoe@gmail.com>
2020-11-12 16:06:59 -0800

    updated wording

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -65,7 +65,7 @@ export default class Visualizer extends Component {
 		return (
 			<div className="canvas-wrapper">
 				<div className="canvas-example-controls">
-					<div>{`MODE: ${mode}`}</div>
+					<div>{`MODE: Random Mood Data`}</div>
 					{/* <Button
 						onClick={() => this.setState({ drawMode: (drawMode + 1) % 2 })}
 						buttonContent={nextModeContent[mode]}
