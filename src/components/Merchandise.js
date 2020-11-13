92b60bdcc33516195b84f5a8dbde1917613ebdb1
mehmehmehlol <hkmeganlo@gmail.com>
2020-11-12 16:22:39 -0800

    i quit messing around with merch :/

--- src/components/Merchandise.js
+++ src/components/Merchandise.js
@@ -4,7 +4,7 @@ const Merchandise = () => {
 	return (
 
 		<div>
-			<img />
+			<img src="https://cdn.shopify.com/s/files/1/0039/7849/4067/products/IMG_1941_600x.jpg?v=1570074291" alt="rose"/>
 		</div>
 		
 	)
